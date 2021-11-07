89627b50ee9debb06585c01fe7cc29ae8e5c19e3
HaiHart <hai.hohartfilli@hcmut.edu.vn>
2021-11-07 18:15:23 +0700

    changing_rest_api

--- server/API/autth.php
+++ server/API/autth.php
@@ -1,6 +1,5 @@
 <?php
 	require_once '../database/dbhelper.php';
-	require '../utils/api_ulti.php';
 	require '../utils/rest_api.php';
 
 	class auth extends rest_api{
@@ -10,34 +9,17 @@
 		function getRole() {
 		$user = getUserToken();
 		if ($user == null) {
-			/*$res = [
-				"status" => -1,
-				"msg" => "Not login!!!",
-				"userList" => [],
-			];
-			echo json_encode($res);*/
 			$this->response(401,"Not login!!");
 			return;
 		}
 		$sql = "select * from role";
 		$result = executeResult($sql);
-		/*$res = [
-			"status" => 1,
-			"msg" => "success!!!",
-			"roleList" => $result,
-		];
-		echo json_encode($res); */
 		$this->response(200,$result);
 	}
 	function login(){
 		$email = $pwd = '';
 		$user = getUserToken();
 		if ($user != null) {
-			/*$res = [
-				"status" => 1,
-				"msg" => "Login successfully!!!",
-				"user" => $user,
-			];*/
 			$this->response(200,$user);
 		} else if (!empty($_POST)) {
 			$email = getPost('email');
@@ -46,10 +28,6 @@
 			$sql = "select * from User where email = '$email' and password = '$pwd'";
 			$userExist = executeResult($sql, true);
 			if ($userExist == null) {
-				/*$res = [
-					"status" => 2,
-					"msg" => "Login failed!!!",
-				];*/
 				$this->response(406,"Login fail");
 			} else {
 				//login successfully
@@ -65,22 +43,11 @@
 				$userId = $userExist['id'];
 				$sql = "insert into tokens (user_id, token, created_at) values ('$userId', '$token', '$created_at')";
 				execute($sql);
-
-				/*$res = [
-					"status" => 1,
-					"msg" => "Login successfully!!!",
-					"user" => $userExist,
-				];*/
 				$this->response(200, $userExist);
 			}
 		} else {
-			/*$res = [
-				"status" => 2,
-				"msg" => "Login failed!!!",
-			];*/
 			$this->response(401,"Login failed");
 		}
-		//echo json_encode($res);
 		return;
 	}
 	function logout(){
@@ -92,18 +59,11 @@
 			execute($sql);
 			setcookie('token', '', time() - 100, '/');
 		}
-
-		/*$res = [
-			"status" => 1,
-			"msg" => "Logout successfully!!!",
-		];*/
 		$this->response(200,"Logout successfully!");
-		//echo json_encode($res);
 		session_destroy();
 	}
 	function register(){
 		$fullname = $email = $phone = $address = '';
-
 		if (!empty($_POST)) {
 			$fullname = getPost('fullname');
 			$email = getPost('email');
@@ -119,10 +79,6 @@
 			} else {
 				$userExist = executeResult("select * from User where email = '$email'", true);
 				if ($userExist != null) {
-					/*$res = [
-						"status" => 2,
-						"msg" => "Register failed!!! Email exists!!!",
-					];*/
 					$this->response(400,"Register failed");
 				} else {
 					$created_at = $updated_at = date('Y-m-d H:i:s');
@@ -136,60 +92,27 @@
 					else{
 							$sql = "insert into User (fullname, email, password, role_id, phone_number, address, created_at, updated_at) values ('$fullname', '$email', '$pwd', 2, '$phone', '$address', '$created_at', '$updated_at')";
 					}
-
-				
 					execute($sql);
-
-					/*$res = [
-						"status" => 1,
-						"msg" => "Register successfully!!!",
-					];*/
 					$this->response(200,"Registered");
 				}
 			}
 		} else {
-			/*$res = [
-				"status" => 2,
-				"msg" => "Register failed!!!",
-			];*/
 			$this->response(400,"Register failed");
 		}
-
-		//echo json_encode($res);
 	}
 	function userList(){
 		$user = getUserToken();
 		if ($user == null) {
-			/*$res = [
-				"status" => -1,
-				"msg" => "Not login!!!",
-				"userList" => [],
-			];
-			
-			echo json_encode($res);*/
 			$this->response(401,"Not login");
 			return;
 		}
 		$sql = "select User.*, Role.name as role_name from User left join Role on User.role_id = Role.id";
 		$data = executeResult($sql);
-
-		/*$res = [
-			"status" => 1,
-			"msg" => "success!!!",
-			"userList" => $data,
-		];
-		echo json_encode($res);*/
 		$this->response(200,$data);
 	}
-
 	function deleteUser(){
 		$user = getUserToken();
 		if ($user == null) {
-			/*$res = [
-				"status" => -1,
-				"msg" => "Not login!!!",
-			];
-			echo json_encode($res);*/
 			$this->response(401,"Not login");
 			return;
 			return;
@@ -217,11 +140,6 @@
 	function editUser(){
 		$user = getUserToken();
 		if ($user == null) {
-			/*$res = [
-				"status" => -1,
-				"msg" => "Not login!!!",
-			];
-			echo json_encode($res);*/
 			$this->response(401,"Not login");
 			return;
 		}
@@ -235,7 +153,6 @@
 			if ($password != '') {
 				$password = getSecurityMD5($password);
 			}
-
 			$created_at = $updated_at = date("Y-m-d H:i:s");
 
 			$role_id = getPost('role_id');
@@ -244,10 +161,6 @@
 			$userItem = executeResult($sql, true);
 
 			if ($userItem != null) {
-				/*$res = [
-					"status" => 2,
-					"msg" => "Email exists!!!",
-				];*/
 				$this->response(409,"Email exists");
 			} else {
 				if ($password != '') {
@@ -256,14 +169,9 @@
 					$sql = "update User set fullname = '$fullname', email = '$email', phone_number = '$phone_number', address = '$address', updated_at = '$updated_at', role_id = $role_id where id = $id";
 				}
 				execute($sql);
-				/*$res = [
-					"status" => 1,
-					"msg" => "Edit success!!!",
-				];*/
 				$this->response(200,"Success!");
 			}
 		}
-		//echo json_encode($res);
 	}
 
 	function addUser(){
@@ -299,21 +207,12 @@
 				//insert
 				$sql = "insert into User(fullname, email, phone_number, address, password, role_id, created_at, updated_at) values ('$fullname', '$email', '$phone_number', '$address', '$password', '$role_id', '$created_at', '$updated_at')";
 				execute($sql);
-				/*$res = [
-					"status" => 1,
-					"msg" => "Add success!!!",
-				];*/
 				$this->response(200,"Success!");
 			} else {
 				//Tai khoan da ton tai -> failed
-				/*$res = [
-					"status" => 2,
-					"msg" => "Email exists!!!",
-				];*/
 				$this->response(409,"Email exists!");
 			}
 		}
-		//echo json_encode($res);
 	}
 
 }
