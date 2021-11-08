c75d9c8c0c2aeff5f67aeaf93274098fdbc85040
nam134778 <62176476+nam134778@users.noreply.github.com>
2021-11-08 23:58:02 +0700

    Modify the responsive of card

--- client/src/components/NewProducts.js
+++ client/src/components/NewProducts.js
@@ -9,7 +9,7 @@ function TopSellingPoducts() {
       <div className="container">
         <div className="row g-2">
           {listProducts.map((product, index) => (
-            <div className="col-lg-3" key={index}>
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <Product product={product} />
             </div>
           ))}
