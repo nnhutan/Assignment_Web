4bfe0c8ebe5516e1222e2d8b19ae204e447ad549
nam134778 <62176476+nam134778@users.noreply.github.com>
2021-11-08 23:56:40 +0700

    Add footer for home page

---
+++ client/src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css"
+function Footer() {
+    return (
+        <footer className="w-100 py-4 flex-shrink-0">
+        <div className="container py-4">
+            <div className="row gy-4 gx-5">
+                <div className="col-lg-3 col-md-6">
+                    <h5 className="h1 text-white">The Bell</h5>
+                    <p className="small text-muted">Công ty TNHH 4 thành viên</p>
+                    <a className="text-primary" href="#">Về trang chủ</a>
+                </div>
+                <div className="col-lg-2 col-md-6">
+                    <h5 className="text-white mb-3">Quick links</h5>
+                    <ul className="list-unstyled text-muted">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Get started</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </div>
+                <div className="col-lg-3 col-md-6">
+                </div>
+                <div className="col-lg-4 col-md-6">
+                    <h5 className="text-white mb-3">Newsletter</h5>
+                    <p className="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                    <form action="#">
+                        <div className="input-group mb-3">
+                            <input className="form-control" type="text" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                            <button className="btn btn-primary" id="button-addon2" type="button"><i className="fas fa-paper-plane"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </footer>
+    );
+}
+export default Footer;
