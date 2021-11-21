d8ed5b5195a2c608b3abf0513dfcc131acb4d756
nam134778 <62176476+nam134778@users.noreply.github.com>
2021-11-21 22:03:13 +0700

    Update News.js

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -1,12 +1,8 @@
-import Header from "../components/Header";
 import NewsPage from "../components/NewsPage";
-import Footer from "../components/Footer";
 function News() {
   return (
     <div className="news-page">
-      <Header currPage="news" />
       <NewsPage />
-      <Footer />
     </div>
   );
 }
