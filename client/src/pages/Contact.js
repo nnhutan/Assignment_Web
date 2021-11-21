556cfcbe9c1e7cc654565e74f6b1ee5f4e9118f2
nam134778 <62176476+nam134778@users.noreply.github.com>
2021-11-21 22:02:25 +0700

    Update Contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,12 +1,8 @@
 import ContactPage from "../components/ContactPage";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 function Contact() {
   return (
     <div className="contact-page">
-      <Header currPage="contact" />
       <ContactPage />
-      <Footer />
     </div>
   );
 }
